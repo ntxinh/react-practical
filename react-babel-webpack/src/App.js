ac425d4583aae2343ced03953fe5845d379dd232
nguyentrucxinh <nguyentrucxjnh@gmail.com>
2019-11-08 18:09:57 +0700

    React svg icon components from cli

--- react-babel-webpack/src/App.js
+++ react-babel-webpack/src/App.js
@@ -1,8 +1,11 @@
 import React from 'react';
-
 import styles from './style.css';
+import FacebookIcon from './Icons/Facebook.js';
 
 const App = ({ title }) =>
-  <div className={styles.title}>{title}</div>;
+  <div className={styles.title}>
+    <FacebookIcon width="40px" height="40px" />
+    {title}
+  </div>;
 
 export default App;
